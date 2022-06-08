f6c333da6f945272f1396a8f90af869454291008
NataliaIv90 <ivantsovana@gmail.com>
2022-06-08 12:31:25 +0300

    Add temperature scale f/c

--- src/index.js
+++ src/index.js
@@ -49,15 +49,48 @@ currentTime();
 // 🕵️‍♀️Feature #2
 // Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
 
+function updateRandomTemperature() {
+  let temperature = Math.random();
+  let scale = document.querySelector("#scale");
+  temperature = temperature * 100 - 50;
+  temperature = Math.round(temperature);
+  document.querySelector("#temperature").innerHTML = temperature;
+}
+
+function updateLocation(inputLocation) {
+  document.querySelector("h1").innerHTML = inputLocation;
+}
+
 function changeLocation(event) {
   event.preventDefault();
   let inputLocation = document.querySelector("#search-city-input");
-  document.querySelector("h1").innerHTML = inputLocation.value;
+  if (inputLocation.value.length > 0) {
+    updateLocation(inputLocation.value);
+    updateRandomTemperature();
+  } else {
+    updateLocation("Khmel'nytsky");
+    updateRandomTemperature();
+    alert("Please, enter a city");
+  }
 }
-
 
 let searchForm = document.querySelector("#location-form");
 searchForm.addEventListener("submit", changeLocation);
 
 // 🙀Bonus Feature
 // Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
+// ℃ ℉
+
+function updateScaleF() {
+  window.location = "./indexF.html";
+}
+
+function updateScale() {
+  window.location = "./index.html";
+}
+
+let buttonF = document.querySelector("#buttonF");
+buttonF.addEventListener("click", updateScale);
+
+let buttonC = document.querySelector("#buttonC");
+buttonF.addEventListener("click", updateScaleF);
